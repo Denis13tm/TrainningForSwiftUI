8890302d9daf6d62f8c4987eb48875490b1d839d
Otabek Tuychiev <otawflash@gmail.com>
2025-04-23 00:01:25 +0900

    20. Conditional Views & 23.Geometry Reader with responsive design (Appstore's Arcade page implemented)

---
+++ TrainningForSwiftUI/ListOfTopicViews/TrainningForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainningForSwiftUIApp.swift
+//  TrainningForSwiftUI
+//
+//  Created by Otabek Tuychiev on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TrainningForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
