b902b271327d7979088bcc15cbdf67b9817567ae
Otabek Tuychiev <otawflash@gmail.com>
2025-04-23 02:01:33 +0900

    21. Transition & Animation View completed

---
+++ TrainningForSwiftUI/ListOfTopicViews/TransitionAndAnimationViews.swift
@@ -0,0 +1,18 @@
+//
+//  TransitionAndAnimationViews.swift
+//  TrainningForSwiftUI
+//
+//  Created by Otabek Tuychiev on 4/23/25.
+//
+
+import SwiftUI
+
+struct TransitionAndAnimationViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransitionAndAnimationViews()
+}
